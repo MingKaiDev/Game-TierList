29187af28788fbf64b9182774c50f733f24ddde3
MingKaiDev <47216870+MingKaiDev@users.noreply.github.com>
2025-05-07 23:19:44 +0800

    Render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const backendURL = import.meta.env.VITE_BACKEND_URL;
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+    fetch(`${backendURL}/api/hello`, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
   }, []);
+
 
   return <h1>{message}</h1>;
 }
--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: "*", // or whitelist like: ["http://localhost:5173"]
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+  }));
+
 app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
