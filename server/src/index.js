2a7500e72652703f4eb212e5f7cd2aa51be6813d
MingKaiDev <47216870+MingKaiDev@users.noreply.github.com>
2025-05-07 23:42:52 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(cors({
-    origin: "*", // or whitelist like: ["http://localhost:5173"]
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
   }));
