d67d0ae7f0a235e1c9983da586cbdf05688a29dc
MingKaiDev <47216870+MingKaiDev@users.noreply.github.com>
2025-05-11 18:15:16 +0800

    Update Blog.jsx

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const initialBlogs = [
   {
@@ -28,7 +29,7 @@ const Blog = () => {
     <div style={styles.container}>
       <div style={styles.header}>
         <h2>Game Blog Posts</h2>
-        <button style={styles.addButton} onClick={() => navigate('/newblog')}>➕ Add New Blog</button>
+        <button style={styles.addButton} onClick={() => useNavigate('/newblog')}>➕ Add New Blog</button>
       </div>
       <div style={styles.blogList}>
         {blogs.map((blog) => (
